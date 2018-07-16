be05c25f41592ddea6571e280ef950f909ea7718
Wan Liuyang <tsfdye@gmail.com>
2018-07-16 15:40:34 +0800

    Add rotate-string.py

---
+++ rotate-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotateString(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: bool
+        """
+
+        return len(A) == len(B) and B in A+A
