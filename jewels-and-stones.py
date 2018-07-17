d0516c55cc04ee8657afd92cbba1040663f37586
sfdye <tsfdye@gmail.com>
2018-07-17 10:44:28 +0800

    Add jewels-and-stones.py

---
+++ jewels-and-stones.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        
+        return sum(map(S.count, J))
