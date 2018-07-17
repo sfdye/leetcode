110d72b306c4ee4dcfcd9d8ede24b8a0763a4891
sfdye <tsfdye@gmail.com>
2018-07-17 10:45:45 +0800

    Add binary-gap.py

---
+++ binary-gap.py
@@ -0,0 +1,10 @@
+class Solution:
+    def binaryGap(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+    
+        idxs = [idx for idx, c in enumerate(bin(N)[2:]) if c == "1"]
+        return max([b - a for a, b in zip(idxs, idxs[1:])] or [0])
+          
