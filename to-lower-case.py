b08cef540554ab8049c7f73fe43613eaf111a4f5
sfdye <tsfdye@gmail.com>
2018-07-17 10:44:59 +0800

    Add to-lower-case.py

---
+++ to-lower-case.py
@@ -0,0 +1,15 @@
+class Solution:
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        
+        ans = ""
+        
+        for c in str:
+            if  "A" <= c <= "Z":
+                ans += chr(ord(c) - (ord("A") - ord("a")))
+            else:
+                ans += c
+        return ans
