a0c3cd49357b76f940d1c9e09aa209c0f05474e3
Wan Liuyang <tsfdye@gmail.com>
2018-07-23 17:38:28 +0800

    Add best-time-to-buy-and-sell-stock-with-transaction-fee.py

---
+++ best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices, fee):
+        """
+        :type prices: List[int]
+        :type fee: int
+        :rtype: int
+        """
+
+        buy = [0] * len(prices)
+        sell = [0] * len(prices)
+
+        buy[0] = - prices[0]
+        for i in range(1, len(prices)):
+            buy[i] = max(buy[i-1], sell[i-1] - prices[i])
+            sell[i] = max(sell[i-1], buy[i-1] + prices[i] - fee)
+        return sell[-1]
