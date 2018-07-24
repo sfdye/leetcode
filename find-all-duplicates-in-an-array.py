763be8f893e8862d0f696821860eacb98c528aed
Wan Liuyang <tsfdye@gmail.com>
2018-07-24 18:13:12 +0800

    Add find-all-duplicates-in-an-array.py

---
+++ find-all-duplicates-in-an-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+        ans = []
+        for num in nums:
+            if nums[abs(num)-1] < 0:
+                ans.append(abs(num))
+            else:
+                nums[abs(num)-1] *= -1
+        return ans
