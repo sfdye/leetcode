76e8f733bc96e0356ce464135f12e05f7973143d
Wan Liuyang <tsfdye@gmail.com>
2018-07-25 17:53:51 +0800

    Add perfect-number.py

---
+++ perfect-number.py
@@ -0,0 +1,18 @@
+import math
+
+
+class Solution:
+    def checkPerfectNumber(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+
+        if num <= 2:
+            return False
+
+        divisor_sum = 1
+        for i in range(2, int(math.sqrt(num))+1):
+            if num % i == 0:
+                divisor_sum += (i + num // i)
+        return divisor_sum == num
