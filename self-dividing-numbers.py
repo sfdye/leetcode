be7b9f0eb76447501e31e2ee2091b5d8c13a31d0
Wan Liuyang <tsfdye@gmail.com>
2018-07-30 15:10:28 +0800

    Add self-dividing-numbers.py

---
+++ self-dividing-numbers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def selfDividingNumbers(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: List[int]
+        """
+
+        def self_dividing(n):
+            for c in str(n):
+                if int(c) == 0:
+                    return False
+                if n % int(c) != 0:
+                    return False
+            return True
+
+        ans = []
+        for num in range(left, right+1):
+            if self_dividing(num):
+                ans.append(num)
+        return ans
