f66072fb5db5e566bb6767cebda615cb583cfdb8
Wan Liuyang <tsfdye@gmail.com>
2018-07-30 15:27:23 +0800

    number-of-lines-to-write-string.py

---
+++ number-of-lines-to-write-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfLines(self, widths, S):
+        """
+        :type widths: List[int]
+        :type S: str
+        :rtype: List[int]
+        """
+
+        ans = [1, 100]
+        for c in S:
+            if widths[ord(c)-ord('a')] <= ans[1]:
+                ans[1] -= widths[ord(c)-ord('a')]
+            else:
+                ans[0] += 1
+                ans[1] = 100 - widths[ord(c)-ord('a')]
+        ans[1] = 100 - ans[1]
+        return ans
