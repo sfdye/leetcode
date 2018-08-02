3f8e72466865e616c4ba4a0ffbe04a4a507671ca
Wan Liuyang <tsfdye@gmail.com>
2018-08-02 16:26:07 +0800

    Add maximum-subarray.py

---
+++ maximum-subarray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        dp = [0] * len(nums)
+        max_sum = dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = nums[i] + max(dp[i-1], 0)
+            max_sum = max(max_sum, dp[i])
+        return max_sum
