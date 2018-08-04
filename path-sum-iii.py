f400d6740cdaefdc989117bdb4ec0fe301ff0e75
Wan Liuyang <tsfdye@gmail.com>
2018-08-04 11:10:58 +0800

    Add path-sum-iii.py

---
+++ path-sum-iii.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def dfs(self, node, target):
+        if not node:
+            return 0
+        return int(node.val == target) + self.dfs(node.left, target-node.val) + self.dfs(node.right, target-node.val)
+
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+
+        if not root:
+            return 0
+        return self.dfs(root, sum) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum)
