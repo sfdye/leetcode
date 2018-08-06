5756249f97921dee0f82d90a6f05358e1b0c6a51
Wan Liuyang <tsfdye@gmail.com>
2018-08-06 23:51:46 +0800

    Add closest-binary-search-tree-value.py

---
+++ closest-binary-search-tree-value.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def closestValue(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: float
+        :rtype: int
+        """
+        self.ans = float("inf")
+
+        while root:
+            if root.val == target:
+                return root.val
+            if abs(root.val-target) < abs(self.ans-target):
+                self.ans = root.val
+            if root.val < target:
+                root = root.right
+            else:
+                root = root.left
+        return self.ans
