9e5b6ea8b2a7b11c4633796a70fb845fe49270e2
Wan Liuyang <tsfdye@gmail.com>
2018-08-06 21:27:31 +0800

    Add binary-search.py

---
+++ binary-search.py
@@ -0,0 +1,21 @@
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        if not nums:
+            return -1
+
+        lo, hi = 0, len(nums) - 1
+        while lo <= hi:
+            mid = (lo+hi) // 2
+            if nums[mid] == target:
+                return mid
+            if target > nums[mid]:
+                lo = mid + 1
+            else:
+                hi = mid-1
+        return -1
