be02dbd2bc78fd74f1d193e9000b198f79def042
Wan Liuyang <tsfdye@gmail.com>
2018-08-06 23:31:14 +0800

    Add two-sum-ii-input-array-is-sorted.py

---
+++ two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        l, r = 0, len(numbers)-1
+        while l < r:
+            if numbers[l] + numbers[r] == target:
+                return [l+1, r+1]
+            elif numbers[l] + numbers[r] < target:
+                l += 1
+            else:
+                r -= 1
