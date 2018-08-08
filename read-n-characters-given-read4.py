824251bed9be918ef6e349a1eff18e2343752133
Wan Liuyang <tsfdye@gmail.com>
2018-08-08 23:57:10 +0800

    Add read-n-characters-given-read4.py

---
+++ read-n-characters-given-read4.py
@@ -0,0 +1,25 @@
+# The read4 API is already defined for you.
+# @param buf, a list of characters
+# @return an integer
+# def read4(buf):
+
+
+class Solution(object):
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Maximum number of characters to read (int)
+        :rtype: The number of characters read (int)
+        """
+
+        buf4 = [""] * 4
+        count = 0
+
+        while n > 0:
+            now = min(n, read4(buf4))
+            if now == 0:
+                break
+            buf[count:count+now+1] = buf4
+            count += now
+            n -= now
+        return count
