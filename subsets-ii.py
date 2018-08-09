45c27225d92447274c2b4201761682f6e94d7f35
Wan Liuyang <tsfdye@gmail.com>
2018-08-09 19:31:47 +0800

    Add subsets-ii.py

---
+++ subsets-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        ans = [[]]
+        nums.sort()
+        for i in range(len(nums)):
+            if i == 0 or nums[i] != nums[i-1]:
+                last = len(ans)
+            for j in range(len(ans) - last, len(ans)):
+                ans.append(ans[j] + [nums[i]])
+        return ans
