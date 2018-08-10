ce39a8359a20d1e1011becac1c0d3ffab211d8d6
Wan Liuyang <tsfdye@gmail.com>
2018-08-10 23:08:52 +0800

    Add valid-palindrome-ii.py

---
+++ valid-palindrome-ii.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        N = len(s)
+        for i in range(N//2):
+            if s[i] != s[N-1-i]:
+                j = N - 1 - i
+                return s[i+1:j+1] == s[i+1:j+1][::-1] or s[i:j] == s[i:j][::-1]
+
+        return True
