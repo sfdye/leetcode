a9e546b6e2b847df0780da5a3c44752eaa454a86
Wan Liuyang <tsfdye@gmail.com>
2018-08-11 22:47:14 +0800

    Add flood-fill.py

---
+++ flood-fill.py
@@ -0,0 +1,29 @@
+class Solution:
+    def floodFill(self, image, sr, sc, newColor):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type newColor: int
+        :rtype: List[List[int]]
+        """
+
+        if not image:
+            return []
+
+        oldColor = image[sr][sc]
+
+        if newColor == oldColor:
+            return image
+
+        N, M = len(image), len(image[0])
+
+        def dfs(i, j):
+            if i < 0 or i >= N or j < 0 or j >= M or image[i][j] != oldColor:
+                return
+            image[i][j] = newColor
+            for k in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                dfs(i+k[0], j+k[1])
+
+        dfs(sr, sc)
+        return image
