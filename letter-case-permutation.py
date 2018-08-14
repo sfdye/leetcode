0564929ed4aab177ab1fd6f37e2971507aa36e8a
Wan Liuyang <tsfdye@gmail.com>
2018-08-14 19:22:34 +0800

    Add letter-case-permutation.py

---
+++ letter-case-permutation.py
@@ -0,0 +1,15 @@
+class Solution:
+    def letterCasePermutation(self, S):
+        """
+        :type S: str
+        :rtype: List[str]
+        """
+
+        ans = [""]
+        for ch in S:
+            if 'a' <= ch.lower() <= 'z':
+                ans = [s+ch.lower() for s in ans] + [s+ch.upper() for s in ans]
+            else:
+                ans = [s+ch for s in ans]
+        return ans
+                    
