6071d2d9d0298db51fc8d108fc1a0e1af592f0ac
Wan Liuyang <tsfdye@gmail.com>
2018-08-14 20:47:58 +0800

    Add 3sum.py

---
+++ 3sum.py
@@ -0,0 +1,31 @@
+class Solution:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        nums.sort()
+        ans = []
+        for i in range(0, len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l = i + 1
+            r = len(nums) - 1 
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s == 0:
+                    ans.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif s < 0:
+                    l += 1
+                else:
+                    r -= 1
+        return ans
+                
+
