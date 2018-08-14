c3e2017d61ee9f69b4f23a58178dfb85ef5dbb8d
Wan Liuyang <tsfdye@gmail.com>
2018-08-14 20:47:32 +0800

    Add 3sum-closest.py

---
+++ 3sum-closest.py
@@ -0,0 +1,22 @@
+class Solution:
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """            
+        nums.sort()
+        ans = float("inf")
+        for i in range(len(nums)-2):
+            l, r = i + 1, len(nums) - 1    
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s == target:
+                    return s
+                elif s < target:
+                    l += 1
+                else:
+                    r -= 1
+                if abs(s-target) < abs(ans-target):
+                    ans = s
+        return ans
