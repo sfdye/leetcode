d1a278ae79ea78e759d62a25b850fc842ae3826c
Wan Liuyang <tsfdye@gmail.com>
2018-08-14 21:18:43 +0800

    Add ransom-note.py

---
+++ ransom-note.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+    
+        return not collections.Counter(ransomNote) - collections.Counter(magazine)
