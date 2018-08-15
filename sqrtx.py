a45d4749f2a0d1218b6ef7e244b9e53ec4496223
Wan Liuyang <tsfdye@gmail.com>
2018-08-15 13:45:59 +0800

    2->3

---
+++ sqrtx.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        if x <= 1:
+            return x
+
+        s = x / 2
+
+        while abs(s ** 2 - x) > 1e-6:
+            s = 0.5 * (s + x / s)
+
+        return int(s)
