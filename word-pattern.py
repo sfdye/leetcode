6b5bcc80e498e53e9ceb7a8e52cc7ac4937c3710
Wan Liuyang <tsfdye@gmail.com>
2018-08-15 23:33:49 +0800

    Add word-pattern.py

---
+++ word-pattern.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        str = str.split()
+        return len(str) == len(pattern) and len(set(pattern)) == len(set(str)) == len(
+            set(zip(pattern, str))
+        )
