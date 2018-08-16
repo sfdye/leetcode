7568d5eb4edad7d895d8cd2972d47478978f7549
Wan Liuyang <tsfdye@gmail.com>
2018-08-16 22:12:48 +0800

    Update maximum-binary-tree.py

--- maximum-binary-tree.py
+++ maximum-binary-tree.py
@@ -13,18 +13,12 @@ class Solution:
         :rtype: TreeNode
         """
 
-        def build(l, r):
-            if l == r:
-                return None
-            else:
-                max_num, max_i = -float("inf"), l
-                for i in range(l, r):
-                    if nums[i] > max_num:
-                        max_num = nums[i]
-                        max_i = i
-                node = TreeNode(max_num)
-                node.left = build(l, max_i)
-                node.right = build(max_i + 1, r)
+        def build(nums):
+            if nums:
+                max_i = nums.index(max(nums))
+                node = TreeNode(nums[max_i])
+                node.left = build(nums[:max_i])
+                node.right = build(nums[max_i+1:])
                 return node
 
-        return build(0, len(nums))
+        return build(nums)
