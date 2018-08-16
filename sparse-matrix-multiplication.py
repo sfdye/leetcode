0782c1bc4790eadd109a599c43bfacf09b27c54f
Wan Liuyang <tsfdye@gmail.com>
2018-08-16 17:03:57 +0800

    Add sparse-matrix-multiplication.py

---
+++ sparse-matrix-multiplication.py
@@ -0,0 +1,15 @@
+class Solution:
+    def multiply(self, A, B):
+        """
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
+
+        C = [[0 for i in range(len(B[0]))] for j in range(len(A))]
+        for i in range(len(A)):
+            for k in range(len(A[0])):
+                if A[i][k] != 0:
+                    for j in range(len(B[0])):
+                        C[i][j] += A[i][k] * B[k][j]
+        return C
