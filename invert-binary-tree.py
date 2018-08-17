a37b61c5a994c77cc3d4c0627c70752551443922
sfdye <tsfdye@gmail.com>
2018-08-17 10:07:02 +0800

    Add invert-binary-tree.py

---
+++ invert-binary-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+
+        def invert(root):
+            if root:
+                self.invertTree(root.left)
+                self.invertTree(root.right)
+                root.left, root.right = root.right, root.left
+                return root
+        
+        return invert(root)
