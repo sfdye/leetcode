60b55c20595fdaa3696a99f2ed7d71d4255641cf
sfdye <tsfdye@gmail.com>
2018-08-18 16:09:41 +0800

    Update wiggle-sort.py

--- wiggle-sort.py
+++ wiggle-sort.py
@@ -5,6 +5,8 @@ class Solution:
         :rtype: void Do not return anything, modify nums in-place instead.
         """
 
-        nums.sort()
-        for i in range(1, len(nums) - 1, 2):
-            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        for i in range(len(nums) - 1):
+            if (i % 2 == 0 and nums[i] > nums[i + 1]) or (
+                i % 2 == 1 and nums[i] < nums[i + 1]
+            ):
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
