9796f38d6051de11c382e10dcb9be566459236cf
Wan Liuyang <tsfdye@gmail.com>
2018-08-19 00:04:14 +0800

    Add top-k-frequent-words.py

---
+++ top-k-frequent-words.py
@@ -0,0 +1,13 @@
+class Solution:
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        return [
+            k
+            for k, _ in sorted(
+                collections.Counter(words).items(), key=lambda x: (-x[1], x[0])
+            )
+        ][:k]
