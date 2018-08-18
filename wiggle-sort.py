f890f4d5cc467bb253c1512c9d9a91daf914ff76
sfdye <tsfdye@gmail.com>
2018-08-18 15:40:06 +0800

    Add wiggle-sort.py

---
+++ wiggle-sort.py
@@ -0,0 +1,10 @@
+class Solution:
+    def wiggleSort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+
+        nums.sort()
+        for i in range(1, len(nums) - 1, 2):
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
