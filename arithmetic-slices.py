ff184210218ef1feb48517965c15207275c1a519
Wan Liuyang <tsfdye@gmail.com>
2018-08-19 18:58:30 +0800

    Add arithmetic-slices.py

---
+++ arithmetic-slices.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfArithmeticSlices(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+
+        dp = [0] * len(A)
+        for i in range(2, len(A)):
+            if A[i] - A[i - 1] == A[i - 1] - A[i - 2]:
+                dp[i] = dp[i - 1] + 1
+        return sum(dp)
