788a584c3a7750d9deefcad66c940e0d981b1fe3
sfdye <tsfdye@gmail.com>
2018-08-22 12:18:11 +0800

    Add baseball-game.py

---
+++ baseball-game.py
@@ -0,0 +1,17 @@
+class Solution:
+    def calPoints(self, ops):
+        """
+        :type ops: List[str]
+        :rtype: int
+        """
+        stack = []
+        for op in ops:
+            if op == "C":
+                stack.pop()
+            elif op == "D":
+                stack.append(stack[-1] * 2)
+            elif op == "+":
+                stack.append(stack[-1] + stack[-2])
+            else:
+                stack.append(int(op))
+        return sum(stack)
