d39966edfa5971e183d9ead2ae843f459efaa60b
sfdye <tsfdye@gmail.com>
2018-08-26 16:35:14 +0800

    Add single-number.py

--- single-number.py
+++ single-number.py
@@ -1,13 +1,10 @@
+import functools
+
+
 class Solution:
-    # @param A, a list of integer
-    # @return an integer
-    def singleNumber(self, A):
-        d = {}
-        for x in A:
-            if not x in d:
-                d[x] = 1
-            else:
-                d[x] += 1
-        for k, v in d.items():
-            if v == 1:
-                return k
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return functools.reduce(operator.xor, nums, 0)
