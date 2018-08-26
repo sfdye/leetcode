dc0e93401f1b801477d01eb89fb0620d9f86d4ff
sfdye <tsfdye@gmail.com>
2018-08-26 18:42:54 +0800

    Add prime-number-of-set-bits-in-binary-representation.py

--- counting-bits.py
+++ counting-bits.py
@@ -1,11 +1,10 @@
-counting-bits/class Solution:
+class Solution:
     def countBits(self, num):
         """
         :type num: int
         :rtype: List[int]
         """
-        ones = [0] * (num+1)
-        for i in range(1, num+1):
-            ones[i] = ones[i & (i-1)] + 1
+        ones = [0] * (num + 1)
+        for i in range(1, num + 1):
+            ones[i] = ones[i & (i - 1)] + 1
         return ones
-        
---
+++ prime-number-of-set-bits-in-binary-representation.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countPrimeSetBits(self, L, R):
+        """
+        :type L: int
+        :type R: int
+        :rtype: int
+        """
+        primes = [2, 3, 5, 7, 11, 13, 17, 19]
+        return sum(bin(x).count("1") in primes for x in range(L, R + 1))
