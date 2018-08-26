352cb5ea4d8095ab00927cd851597887d2c08397
sfdye <tsfdye@gmail.com>
2018-08-26 18:12:02 +0800

    Update binary-number-with-alternating-bits.py

--- binary-number-with-alternating-bits.py
+++ binary-number-with-alternating-bits.py
@@ -4,5 +4,5 @@ class Solution:
         :type n: int
         :rtype: bool
         """
-        bits = bin(n)[2:]
-        return all(bits[i] != bits[i + 1] for i in range(len(bits) - 1))
+        a = n ^ (n >> 1)
+        return a & (a + 1) == 0
