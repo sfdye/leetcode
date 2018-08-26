f8daf57bd84b4616e6362455a1001f0a2ef15932
sfdye <tsfdye@gmail.com>
2018-08-26 17:52:03 +0800

    Update power-of-two.py

--- power-of-two.py
+++ power-of-two.py
@@ -4,11 +4,4 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-
-        if n <= 0:
-            return False
-
-        while n % 2 == 0:
-            n /= 2
-
-        return n == 1
+        return n > 0 and n & (n - 1) == 0
