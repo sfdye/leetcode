6a6d33b34157c456def4ca01f30f4da7aab3cdfa
sfdye <tsfdye@gmail.com>
2018-08-29 00:02:21 +0800

    Add total-hamming-distance.py

---
+++ total-hamming-distance.py
@@ -0,0 +1,16 @@
+class Solution:
+    def totalHammingDistance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ones = [0 for _ in range(32)]
+        N = len(nums)
+        for num in nums:
+            i = 0
+            while num:
+                if num & 1:
+                    ones[i] += 1
+                i += 1
+                num >>= 1
+        return sum(ones[i] * (N - ones[i]) for i in range(32))
