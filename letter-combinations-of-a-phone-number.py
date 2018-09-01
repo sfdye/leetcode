92b66e80fc92ce44e0aace5bc4e16a5e24b7481a
sfdye <tsfdye@gmail.com>
2018-09-01 23:49:22 +0800

    Update letter-combinations-of-a-phone-number.py

--- letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number.py
@@ -4,21 +4,10 @@ class Solution:
         :type digits: str
         :rtype: List[str]
         """
-
-        m = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
-
-        self.ans = []
-
-        def dfs(s, cur):
-            if len(s) == 0:
-                self.ans.append(cur)
-                return
-            else:
-                for c in m[s[0]]:
-                    dfs(s[1:], cur + c)
-
         if not digits:
             return []
-
-        dfs(digits, "")
-        return self.ans
+        letters = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+        ans = [""]
+        for d in digits:
+            ans = [s + letter for letter in letters[int(d)] for s in ans]
+        return ans
