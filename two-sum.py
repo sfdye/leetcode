8ca1f616be16b54ada7ee313d1637fe31dd1093b
sfdye <tsfdye@gmail.com>
2018-09-01 12:53:09 +0800

    Update two-sum.py

--- two-sum.py
+++ two-sum.py
@@ -1,21 +1,13 @@
-from collections import defaultdict
-
-
-class Solution(object):
+class Solution:
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-
-        d = defaultdict(list)
-        for i, num in enumerate(nums):
-            d[num].append(i)
-
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in d:
-                for x in d[diff]:
-                    if x != i:
-                        return [x, i]
+        d = {}
+        for idx, num in enumerate(nums):
+            if target - num in d:
+                return [d[target - num], idx]
+            else:
+                d[num] = idx
