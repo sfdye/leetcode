ad82723dabdc927a51bfaee734f88829c4936449
sfdye <tsfdye@gmail.com>
2018-09-01 22:48:25 +0800

    Update integer-to-roman.py

--- integer-to-roman.py
+++ integer-to-roman.py
@@ -4,42 +4,10 @@ class Solution:
         :type num: int
         :rtype: str
         """
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
         ans = ""
-        while num:
-            if num > 999:
-                ans += "M" * (num // 1000)
-                num %= 1000
-            elif num > 99:
-                d = num // 100
-                if 1 <= d < 4:
-                    ans += "C" * d
-                elif d == 4:
-                    ans += "CD"
-                elif 4 < d <= 8:
-                    ans += "D" + "C" * (d - 5)
-                else:
-                    ans += "CM"
-                num %= 100
-            elif num > 9:
-                d = num // 10
-                if 1 <= d < 4:
-                    ans += "X" * d
-                elif d == 4:
-                    ans += "XL"
-                elif 4 < d <= 8:
-                    ans += "L" + "X" * (d - 5)
-                else:
-                    ans += "XC"
-                num %= 10
-            else:
-                d = num
-                if 1 <= d < 4:
-                    ans += "I" * d
-                elif d == 4:
-                    ans += "IV"
-                elif 4 < d <= 8:
-                    ans += "V" + "I" * (d - 5)
-                else:
-                    ans += "IX"
-                num %= 1
+        for v, r in zip(values, romans):
+            ans += num // v * r
+            num %= v
         return ans
