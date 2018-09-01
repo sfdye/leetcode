1130b29806850a3464bba14b9397529f1125b1cb
sfdye <tsfdye@gmail.com>
2018-09-02 00:29:21 +0800

    Update merge-two-sorted-lists.py

--- merge-two-sorted-lists.py
+++ merge-two-sorted-lists.py
@@ -1,25 +1,26 @@
-class Solution(object):
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
     def mergeTwoLists(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
-
         dummy = ListNode(0)
-        tmp = dummy
-        while l1 != None and l2 != None:
-            if l1.val < l2.val:
-                tmp.next = l1
+        node = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                node.next = l1
                 l1 = l1.next
             else:
-                tmp.next = l2
+                node.next = l2
                 l2 = l2.next
-            tmp = tmp.next
-
-        if l1 != None:
-            tmp.next = l1
-        else:
-            tmp.next = l2
-
+            node = node.next
+        node.next = l1 or l2
         return dummy.next
