abf0df06a3c254bdf3c8e853bf73a3c818b454d7
sfdye <tsfdye@gmail.com>
2018-09-02 16:24:06 +0800

    Update remove-duplicates-from-sorted-array.py

--- remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array.py
@@ -6,9 +6,9 @@ class Solution:
         """
         if not nums:
             return 0
-        i = 0
-        for j in range(1, len(nums)):
-            if nums[j] != nums[i]:
-                i += 1
-                nums[i] = nums[j]
-        return i + 1
+        slow = 0
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+        return slow + 1
