812e18069ebf91ade2c4c1ae10249299179f8787
sfdye <tsfdye@gmail.com>
2018-09-02 16:33:02 +0800

    Update implement-strstr.py

--- implement-strstr.py
+++ implement-strstr.py
@@ -1,8 +1,13 @@
-class Solution(object):
+class Solution:
     def strStr(self, haystack, needle):
         """
         :type haystack: str
         :type needle: str
         :rtype: int
         """
-        return haystack.find(needle)
+        if not needle:
+            return 0
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i : i + len(needle)] == needle:
+                return i
+        return -1
