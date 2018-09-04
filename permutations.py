64f05161f91340172da9ca6ff40377c2d3f9f84a
Wan Liuyang <tsfdye@gmail.com>
2018-09-04 08:55:54 +0800

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -1,5 +1,17 @@
 class Solution:
-    # @param num, a list of integer
-    # @return a list of lists of integers
-    def permute(self, num):
-        return [list(x) for x in itertools.permutations(num)]
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans = []
+
+        def dfs(nums, cur):
+            if not nums:
+                ans.append(cur)
+                return
+            for i in range(len(nums)):
+                dfs(nums[:i] + nums[i + 1 :], cur + [nums[i]])
+
+        dfs(nums, [])
+        return ans
