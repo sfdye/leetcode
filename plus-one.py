54ccde5ca8cdb693fd92d71c8a570f8fce3f09a4
Wan Liuyang <tsfdye@gmail.com>
2018-09-04 23:00:43 +0800

    Update plus-one.py

--- plus-one.py
+++ plus-one.py
@@ -1,16 +1,12 @@
 class Solution:
-    # @param digits, a list of integer digits
-    # @return a list of integer digits
     def plusOne(self, digits):
-        digits[len(digits) - 1] += 1
-        x = len(digits) - 1
-        while x > 0 and digits[x] == 10:
-            digits[x - 1] += 1
-            digits[x] = 0
-            x -= 1
-
-        if digits[0] == 10:
-            digits[0] = 0
-            digits.insert(0, 1)
-
-        return digits
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(digits)):
+            if digits[~i] < 9:
+                digits[~i] += 1
+                return digits
+            digits[~i] = 0
+        return [1] + [0] * len(digits)
