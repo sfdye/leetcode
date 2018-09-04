a6cae746dc8c8d2aae437b908b124593d59e6081
Wan Liuyang <tsfdye@gmail.com>
2018-09-04 08:45:41 +0800

    Update multiply-strings.py

--- multiply-strings.py
+++ multiply-strings.py
@@ -5,13 +5,12 @@ class Solution:
         :type num2: str
         :rtype: str
         """
-
-        ans = [0] * (len(num1) + len(num2))
-        for i, n1 in enumerate(num1[::-1]):
-            for j, n2 in enumerate(num2[::-1]):
+        ans = [0 for _ in range(len(num1) + len(num2))]
+        for i, n1 in enumerate(reversed(num1)):
+            for j, n2 in enumerate(reversed(num2)):
                 ans[i + j] += int(n1) * int(n2)
                 ans[i + j + 1] += ans[i + j] // 10
-                ans[i + j] %= 10
+                ans[i + j] = ans[i + j] % 10
         while len(ans) > 1 and ans[-1] == 0:
             ans.pop()
         return "".join(map(str, ans[::-1]))
