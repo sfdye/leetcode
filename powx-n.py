ff093e113a4b9f5fdd69ef888b3882614409760c
Wan Liuyang <tsfdye@gmail.com>
2018-09-04 09:11:00 +0800

    Update powx-n.py

--- powx-n.py
+++ powx-n.py
@@ -1,6 +1,16 @@
 class Solution:
-    # @param x, a float
-    # @param n, a integer
-    # @return a float
-    def pow(self, x, n):
-        return x ** n
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        minus = n < 0
+        n = abs(n)
+        ans, p = 1, x
+        while n:
+            if n & 1:
+                ans *= p
+            p *= p
+            n >>= 1
+        return 1.0 / ans if minus else ans
