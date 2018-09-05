7792d2cf67a5a27bb6fd2904f2477105ece10bb4
Wan Liuyang <tsfdye@gmail.com>
2018-09-05 21:48:49 +0800

    Update word-search.py

--- word-search.py
+++ word-search.py
@@ -5,29 +5,25 @@ class Solution:
         :type word: str
         :rtype: bool
         """
-
         if not board:
             return False
 
-        N, M = len(board), len(board[0])
+        n, m = len(board), len(board[0])
 
-        def dfs(i, j, word, board, cur):
-            if cur == len(word):
+        def dfs(i, j, word):
+            if word == "":
                 return True
-            if i < 0 or i >= N or j < 0 or j >= M or board[i][j] != word[cur]:
+            if i < 0 or i >= n or j < 0 or j >= m or board[i][j] != word[0]:
                 return False
             board[i][j] = "#"
             ret = (
-                dfs(i + 1, j, word, board, cur + 1)
-                or dfs(i, j + 1, word, board, cur + 1)
-                or dfs(i - 1, j, word, board, cur + 1)
-                or dfs(i, j - 1, word, board, cur + 1)
+                dfs(i - 1, j, word[1:]) or dfs(i, j + 1, word[1:]) or dfs(i + 1, j, word[1:]) or dfs(i, j - 1, word[1:])
             )
-            board[i][j] = word[cur]
+            board[i][j] = word[0]
             return ret
 
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if dfs(i, j, word, board, 0):
+        for i in range(n):
+            for j in range(m):
+                if dfs(i, j, word):
                     return True
         return False
