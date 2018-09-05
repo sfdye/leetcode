e7ebd6a511808550ac7a38d6fa2547d0c23d3810
Wan Liuyang <tsfdye@gmail.com>
2018-09-05 22:49:24 +0800

    Update merge-sorted-array.py

--- merge-sorted-array.py
+++ merge-sorted-array.py
@@ -7,15 +7,14 @@ class Solution:
         :type n: int
         :rtype: void Do not return anything, modify nums1 in-place instead.
         """
-
         k = m + n - 1
-        while m > 0 and n > 0:
-            if nums1[m - 1] > nums2[n - 1]:
+        while m and n:
+            if nums1[m - 1] >= nums2[n - 1]:
                 nums1[k] = nums1[m - 1]
                 m -= 1
             else:
                 nums1[k] = nums2[n - 1]
                 n -= 1
             k -= 1
-        if n > 0:
+        if n:
             nums1[:n] = nums2[:n]
