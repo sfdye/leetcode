98d0c1a501225762438037e96d8214287f33e483
Wan Liuyang <tsfdye@gmail.com>
2018-09-07 21:42:10 +0800

    Update symmetric-tree.py

--- symmetric-tree.py
+++ symmetric-tree.py
@@ -1,23 +1,24 @@
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 
-class Solution(object):
+class Solution:
     def isSymmetric(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        return self.is_mirror(root, root)
 
-    def is_mirror(self, t1, t2):
-        if t1 == None and t2 == None:
-            return True
-        elif t1 == None or t2 == None:
-            return False
-        else:
-            return t1.val == t2.val and self.is_mirror(t1.left, t2.right) and self.is_mirror(t1.right, t2.left)
+        def is_mirror(t1, t2):
+            if not t1 and not t2:
+                return True
+            elif t1 and t2 and t1.val == t2.val:
+                return is_mirror(t1.left, t2.right) and is_mirror(t1.right, t2.left)
+            else:
+                return False
+
+        return not root or is_mirror(root.left, root.right)
