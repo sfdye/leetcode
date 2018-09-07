92c4377051035e2d1d6638f483534a0bb795cad5
Wan Liuyang <tsfdye@gmail.com>
2018-09-07 21:01:32 +0800

    Add longest-palindrome.py

---
+++ longest-palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        odds = sum(v & 1 for v in collections.Counter(s).values())
+        return len(s) - odds + bool(odds)
