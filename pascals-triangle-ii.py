86eef649cef238b57c911abf5edf3a602fd6fbd8
Wan Liuyang <tsfdye@gmail.com>
2018-09-08 15:17:22 +0800

    Update pascals-triangle-ii.py

--- pascals-triangle-ii.py
+++ pascals-triangle-ii.py
@@ -1,18 +1,10 @@
-class Solution(object):
+class Solution:
     def getRow(self, rowIndex):
         """
         :type rowIndex: int
         :rtype: List[int]
         """
-        a = [1]
-        for i in range(2, rowIndex + 2):
-            b = [0] * i
-            for j in range(0, i):
-                if j == 0 or j == i - 1:
-                    b[j] = 1
-                else:
-                    b[j] = a[j - 1] + a[j]
-
-            a, b = b, a
-
-        return a
+        row = [1]
+        for i in range(rowIndex):
+            row = [x + y for x, y in zip([0] + row, row + [0])]
+        return row
