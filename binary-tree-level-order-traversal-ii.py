7bc61f2019b2b7cfe10fdb7746ef1173267ff47e
Wan Liuyang <tsfdye@gmail.com>
2018-09-08 08:27:48 +0800

    Update binary-tree-level-order-traversal-ii.py

--- binary-tree-level-order-traversal-ii.py
+++ binary-tree-level-order-traversal-ii.py
@@ -1,35 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
 
-class Solution(object):
-    def dfs(self, root, depth):
-
-        if root == None:
-            return
-        else:
-            if len(ans) <= depth:
-                ans.append([root.val])
-            else:
-                ans[depth].append(root.val)
-
-            self.dfs(root.left, depth + 1)
-            self.dfs(root.right, depth + 1)
-
+class Solution:
     def levelOrderBottom(self, root):
         """
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-
-        global ans
-
-        ans = []
-
-        self.dfs(root, 0)
-
+        level, ans = [root], []
+        while root and level:
+            ans.append([node.val for node in level])
+            level = [kid for node in level for kid in (node.left, node.right) if kid]
         return ans[::-1]
