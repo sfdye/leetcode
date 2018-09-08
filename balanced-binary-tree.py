3f45bc96410d4d97981abb914f496ec9835fbf21
Wan Liuyang <tsfdye@gmail.com>
2018-09-08 10:35:57 +0800

    Update balanced-binary-tree.py

--- balanced-binary-tree.py
+++ balanced-binary-tree.py
@@ -1,24 +1,24 @@
-class Solution(object):
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
     def isBalanced(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
+        return (
+            abs(self.height(root.left) - self.height(root.right)) <= 1
+            and self.isBalanced(root.left)
+            and self.isBalanced(root.right)
+            if root
+            else True
+        )
 
-        balanced, _ = self.validate(root)
-
-        return balanced
-
-    def validate(self, root):
-        if not root:
-            return True, 0
-
-        balanced, leftHeight = self.validate(root.left)
-        if not balanced:
-            return False, 0
-
-        balanced, rightHeight = self.validate(root.right)
-        if not balanced:
-            return False, 0
-
-        return abs(leftHeight - rightHeight) <= 1, max(leftHeight, rightHeight) + 1
+    def height(self, node):
+        return 1 + max(self.height(node.left), self.height(node.right)) if node else 0
