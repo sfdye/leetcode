5ae10d14b39bacf366fd879f1974009106df8d16
Wan Liuyang <tsfdye@gmail.com>
2018-09-12 09:29:49 +0800

    Update sort-list.py

--- sort-list.py
+++ sort-list.py
@@ -1,23 +1,37 @@
-class Solution(object):
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
     def sortList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
+        if not head or not head.next:
+            return head
+        pre = slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            pre = slow
+            slow = slow.next
+        pre.next = None
+        left = self.sortList(head)
+        right = self.sortList(slow)
+        return self.merge(left, right)
 
-        l = []
-        node = head
-        while node:
-            l.append(node.val)
-            node = node.next
-
-        l.sort()
-
-        dummy = ListNode(0)
-        p = dummy
-        for x in l:
-            q = ListNode(x)
-            p.next = q
-            p = q
-
+    def merge(self, left, right):
+        dummy = cur = ListNode(None)
+        while left and right:
+            if left.val < right.val:
+                cur.next = left
+                left = left.next
+            else:
+                cur.next = right
+                right = right.next
+            cur = cur.next
+        cur.next = left or right
         return dummy.next
