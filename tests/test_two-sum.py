15339bc4c7fa5b7dead6e576cea194323f60ba02
Wan Liuyang <tsfdye@gmail.com>
2018-09-14 16:36:57 +0800

    Restructure the tests

--- tests/test_two-sum.py
+++ tests/test_two-sum.py
@@ -5,7 +5,7 @@ import importlib
 
 class TestCases(unittest.TestCase):
     def setUp(self):
-        self.s = importlib.import_module("two-sum").Solution()
+        self.s = importlib.import_module("leetcode.two-sum").Solution()
 
     def test_two_nums(self):
         self.assertEqual(self.s.twoSum([1, 2], 3), [0, 1])
