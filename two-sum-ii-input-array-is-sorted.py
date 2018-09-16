dd59d873123a5af183c652a4b95130579de6faf4
Wan Liuyang <tsfdye@gmail.com>
2018-09-16 22:36:36 +0800

    Update two-sum-ii-input-array-is-sorted.py

--- two-sum-ii-input-array-is-sorted.py
+++ two-sum-ii-input-array-is-sorted.py
@@ -5,12 +5,12 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-
-        l, r = 0, len(numbers) - 1
-        while l < r:
-            if numbers[l] + numbers[r] == target:
-                return [l + 1, r + 1]
-            elif numbers[l] + numbers[r] < target:
-                l += 1
+        lo, hi = 0, len(numbers) - 1
+        while lo < hi:
+            s = numbers[lo] + numbers[hi]
+            if s == target:
+                return [lo + 1, hi + 1]
+            elif s < target:
+                lo += 1
             else:
-                r -= 1
+                hi -= 1
