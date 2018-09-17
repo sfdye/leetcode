feaf6d5df37b64fafc41dc3d2a2c14c213d7a8e1
Wan Liuyang <tsfdye@gmail.com>
2018-09-17 23:04:24 +0800

    Add fraction-to-recurring-decimal.py

---
+++ fraction-to-recurring-decimal.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def fractionToDecimal(self, numerator, denominator):
+        """
+        :type numerator: int
+        :type denominator: int
+        :rtype: str
+        """
+        n, remainder = divmod(abs(numerator), abs(denominator))
+        sign = "-" if numerator * denominator < 0 else ""
+        ans = [sign + str(n), "."]
+        stack = []
+        while remainder not in stack:
+            stack.append(remainder)
+            n, remainder = divmod(abs(remainder) * 10, abs(denominator))
+            ans.append(str(n))
+        idx = stack.index(remainder)
+        ans.insert(idx + 2, "(")
+        ans.append(")")
+        return "".join(ans).replace("(0)", "").rstrip(".")
+
