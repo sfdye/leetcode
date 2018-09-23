4a2cf36687848694e743a893ef8d6cfcecbda70b
Wan Liuyang <tsfdye@gmail.com>
2018-09-23 16:33:41 +0800

    Update string-compression.py

--- string-compression.py
+++ string-compression.py
@@ -1,19 +1,19 @@
-class Solution(object):
+class Solution:
     def compress(self, chars):
         """
         :type chars: List[str]
         :rtype: int
         """
-
-        write = anchor = 0
-
+        anchor = write = 0
         for read, c in enumerate(chars):
-            if read + 1 == len(chars) or chars[read + 1] != c:
+            if read == len(chars) - 1 or c != chars[read + 1]:
                 chars[write] = chars[anchor]
                 write += 1
-                if read > anchor:
-                    for digit in str(read - anchor + 1):
-                        chars[write] = digit
+                count = read - anchor + 1
+                if count > 1:
+                    for d in str(count):
+                        chars[write] = d
                         write += 1
                 anchor = read + 1
         return write
+
