3d6319fd6e1e36fa5bbf1713ae95344f78af45f2
Wan Liuyang <tsfdye@gmail.com>
2018-09-26 15:57:55 +0800

    Update trapping-rain-water.py

--- trapping-rain-water.py
+++ trapping-rain-water.py
@@ -4,30 +4,15 @@ class Solution:
         :type height: List[int]
         :rtype: int
         """
-
-        if not height:
-            return 0
-
-        max_value = max(height)
-        max_index = height.index(max_value)
-        total = max_value * len(height)
-        left = 0
-        right = len(height) - 1
-        mid = max_index
-        cur = left + 1
-        untraped = 0
-        while cur <= mid:
-            while cur < mid and height[cur] <= height[left]:
-                cur += 1
-            untraped += (height[cur] - height[left]) * cur
-            left = cur
-            cur += 1
-
-        cur = right - 1
-        while mid <= cur:
-            while mid < cur and height[cur] <= height[right]:
-                cur -= 1
-            untraped += (height[cur] - height[right]) * (len(height) - cur - 1)
-            right = cur
-            cur -= 1
-        return total - untraped - sum(height)
+        left, right = 0, len(height) - 1
+        ans = left_max = right_max = 0
+        while left < right:
+            if height[left] < height[right]:
+                left_max = max(left_max, height[left])
+                ans += left_max - height[left]
+                left += 1
+            else:
+                right_max = max(right_max, height[right])
+                ans += right_max - height[right]
+                right -= 1
+        return ans
