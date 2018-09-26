1f5a2f1c2372293f3c5b5af44017e0bf6ac91390
Wan Liuyang <tsfdye@gmail.com>
2018-09-26 10:11:46 +0800

    Update number-of-islands.py

--- number-of-islands.py
+++ number-of-islands.py
@@ -1,45 +1,26 @@
-class Solution(object):
-    def dfs(self, grid, x, y):
-
-        if visited[x][y] == 1:
-            return
-        visited[x][y] = 1
-
-        if 0 <= x - 1 < n and 0 <= y < m and grid[x - 1][y] == "1":
-            self.dfs(grid, x - 1, y)
-
-        if 0 <= x + 1 < n and 0 <= y < m and grid[x + 1][y] == "1":
-            self.dfs(grid, x + 1, y)
-
-        if 0 <= x < n and 0 <= y - 1 < m and grid[x][y - 1] == "1":
-            self.dfs(grid, x, y - 1)
-
-        if 0 <= x < n and 0 <= y + 1 < m and grid[x][y + 1] == "1":
-            self.dfs(grid, x, y + 1)
-
+class Solution:
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
-        global n, m
-
-        n = len(grid)
-
-        if n == 0:
+        if not grid:
             return 0
 
-        m = len(grid[0])
+        count = 0
+        r, c = len(grid), len(grid[0])
 
-        global visited
+        def dfs(i, j):
+            if i < 0 or i >= r or j < 0 or j >= c or grid[i][j] == "0":
+                return
+            grid[i][j] = "0"
+            for d in ((-1, 0), (0, 1), (1, 0), (0, -1)):
+                dfs(i + d[0], j + d[1])
 
-        visited = [[0] * m for i in range(n)]
+        for i in range(r):
+            for j in range(c):
+                if grid[i][j] == "1":
+                    dfs(i, j)
+                    count += 1
+        return count
 
-        ans = 0
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == "1" and visited[i][j] == 0:
-                    ans += 1
-                    self.dfs(grid, i, j)
-
-        return ans
