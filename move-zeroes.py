947c03796302b16aeaa9d8678fc2a255057e6fc6
Wan Liuyang <tsfdye@gmail.com>
2018-10-04 20:50:36 +0800

    Update move-zeroes.py

--- move-zeroes.py
+++ move-zeroes.py
@@ -1,11 +1,14 @@
-class Solution:
+class Solution(object):
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        k = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[k], nums[i] = nums[i], nums[k]
-                k += 1
+        fast = slow = 0
+        while fast < len(nums):
+            if nums[fast] != 0:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        nums[slow:] = [0] * (len(nums)-slow)
+        
