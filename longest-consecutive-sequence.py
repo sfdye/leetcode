4d36971bba202734cf31b15c325ca2557b2e3cc8
Wan Liuyang <tsfdye@gmail.com>
2018-10-04 22:08:35 +0800

    Add longest-consecutive-sequence.py

---
+++ longest-consecutive-sequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        longest_streak = 0
+        nums = set(nums)
+        for num in nums:
+            if num - 1 not in nums:
+                cur = num
+                cur_streak = 1
+                while cur + 1  in nums:
+                    cur_streak += 1
+                    cur += 1
+                longest_streak = max(longest_streak, cur_streak)
+        return longest_streak
