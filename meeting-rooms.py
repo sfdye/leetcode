a904d9ef466abbf177199e1e8d1daa3e87755577
Wan Liuyang <tsfdye@gmail.com>
2018-10-06 00:14:02 +0800

    Update meeting-rooms.py

--- meeting-rooms.py
+++ meeting-rooms.py
@@ -11,9 +11,6 @@ class Solution:
         :type intervals: List[Interval]
         :rtype: bool
         """
-
         intervals.sort(key=lambda x: x.start)
-        for i in range(1, len(intervals)):
-            if intervals[i].start < intervals[i - 1].end:
-                return False
-        return True
+        return all(intervals[i].start >= intervals[i - 1].end for i in range(1, len(intervals)))
+
