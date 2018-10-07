0ec82df8bd2a4bbf3519874ac9aca46ff1280af2
Wan Liuyang <tsfdye@gmail.com>
2018-10-08 00:07:01 +0800

    Update basic-calculator-ii.py

--- basic-calculator-ii.py
+++ basic-calculator-ii.py
@@ -5,10 +5,11 @@ class Solution:
         :rtype: int
         """
         num, stack, sign = 0, [], "+"
+        s += " "
         for i in range(len(s)):
             if s[i].isdigit():
                 num = num * 10 + int(s[i])
-            if s[i] in "+-*/" or i == len(s) - 1:
+            elif s[i] in "+-*/" or i == len(s) - 1:
                 if sign == "+":
                     stack.append(num)
                 elif sign == "-":
