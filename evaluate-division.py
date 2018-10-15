0b5ddfd2d14441644a25066e50a7934624673275
Wan Liuyang <tsfdye@gmail.com>
2018-10-15 22:04:56 +0800

    Update evaluate-division.py

--- evaluate-division.py
+++ evaluate-division.py
@@ -7,12 +7,16 @@ class Solution:
         :rtype: List[float]
         """
         d = collections.defaultdict(dict)
-        for (x, y), v in zip(equations, values):
-            d[x][y] = v
-            d[y][x] = 1.0 / v
-            d[x][x] = d[y][y] = 1.0
+        for (a, b), v in zip(equations, values):
+            d[a][b] = v
+            d[b][a] = 1.0 / v
+            d[a][a] = d[b][b] = 1.0
         for k in d:
             for i in d[k]:
                 for j in d[k]:
                     d[i][j] = d[i][k] * d[k][j]
-        return [d[x].get(y, -1.0) for x, y in queries]
+        return [d[a].get(b, -1.0) for a, b in queries]
+            
+            
+        
+                        
