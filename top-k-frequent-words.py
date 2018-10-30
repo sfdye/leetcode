c302443777832f8f8c574d03cd1fc66e6a7565d2
Wan Liuyang <tsfdye@gmail.com>
2018-10-30 10:55:17 +0800

    Update top-k-frequent-elements.py

--- top-k-frequent-words.py
+++ top-k-frequent-words.py
@@ -1,16 +1,28 @@
 class Solution:
     def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        feq = collections.defaultdict(list)
-        for key, count in collections.Counter(nums).items():
-            feq[count].append(key)
-        res = []
-        for i in range(len(nums) + 1, -1, -1):
-            res.extend(feq[i])
-            if len(res) >= k:
-                break
-        return res[:k]
+        def quick_select(l, r):
+            pivot = l
+            i, j = l, r
+            while i < j:
+                while i < j and counts[j][1] <= counts[pivot][1]:
+                    j -= 1
+                while i < j and counts[i][1] >= counts[pivot][1]:
+                    i += 1
+                counts[i], counts[j] = counts[j], counts[i]
+            counts[l], counts[i] = counts[i], counts[l]
+
+            if i + 1 == k:
+                return counts[: i + 1]
+            elif i + 1 < k:
+                return quick_select(i + 1, r)
+            else:
+                return quick_select(l, i - 1)
+
+        if not nums:
+            return []
+
+        counts = collections.defaultdict(int)
+        for num in nums:
+            counts[num] += 1
+        counts = list(counts.items())
+        return [c[0] for c in quick_select(0, len(counts) - 1)]
