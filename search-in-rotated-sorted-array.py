07a9c615a6a7333b265a621b2a0d9c1f056a8275
Wan Liuyang <tsfdye@gmail.com>
2019-02-09 22:41:51 +0800

    Update search-in-rotated-sorted-array.py

--- search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array.py
@@ -11,8 +11,41 @@ class Solution(object):
             mid = (lo + hi) // 2
             if nums[mid] == target:
                 return mid
-            if nums[0] <= target < nums[mid] or target < nums[mid] < nums[0] or nums[mid] < nums[0] <= target:
-                hi = mid - 1
+            if nums[mid] >= nums[lo]:
+                if nums[lo] <= target < nums[mid]:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
             else:
-                lo = mid + 1
+                if nums[mid] < target <= nums[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+
+        return -1
+
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        lo, hi = 0, len(nums) - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] >= nums[lo]:
+                if nums[lo] <= target < nums[mid]:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            else:
+                if nums[mid] < target <= nums[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
         return -1
