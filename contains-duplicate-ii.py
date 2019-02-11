ec04daf5f93ad02f553fc3421376d9f67764df5e
Wan Liuyang <tsfdye@gmail.com>
2019-02-11 23:42:55 +0800

    Update contains-duplicate-ii.py

--- contains-duplicate-ii.py
+++ contains-duplicate-ii.py
@@ -1,25 +1,8 @@
-class T:
-    def __init__(self, num, pos):
-        self.num = num
-        self.pos = pos
-
-    def __str__(self):
-        return "num={}, pos={}".format(self.num, self.pos)
-
-
-class Solution(object):
-    def containsNearbyDuplicate(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
-        """
-
-        a = [T(num, index) for index, num in enumerate(nums)]
-        a.sort(key=lambda x: (x.num, x.pos))
-
-        for i in range(len(a) - 1):
-            if a[i].num == a[i + 1].num and a[i + 1].pos - a[i].pos <= k:
+class Solution:
+    def containsNearbyDuplicate(self, nums: "List[int]", k: "int") -> "bool":
+        d = {}
+        for i, num in enumerate(nums):
+            if num in d and i - d[num] <= k:
                 return True
-
+            d[num] = i
         return False
