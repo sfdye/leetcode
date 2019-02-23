8412bc9da2175d15dbbfd80aff20dc77742c7a39
Wan Liuyang <tsfdye@gmail.com>
2019-02-23 23:45:09 +0800

    Update next-permutation.py

--- next-permutation.py
+++ next-permutation.py
@@ -1,18 +1,17 @@
 class Solution:
-    def nextPermutation(self, nums):
+    def nextPermutation(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-
-        i = len(nums) - 1
-        while i > 0 and nums[i] <= nums[i - 1]:
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
             i -= 1
-        if i == 0:
+        if i < 0:
             nums.reverse()
         else:
-            for j in range(len(nums) - 1, -1, -1):
-                if nums[j] > nums[i - 1]:
-                    nums[i - 1], nums[j] = nums[j], nums[i - 1]
-                    break
-            nums[i:] = nums[i:][::-1]
+            j = len(nums) - 1
+            while j >= 0 and nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+            nums[i + 1 :] = nums[i + 1 :][::-1]
+
