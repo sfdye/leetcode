16a6e9a16109d94123286412bfb894b8ba0f2dfc
Wan Liuyang <tsfdye@gmail.com>
2019-03-02 15:19:57 +0800

    Update pascals-triangle.py

--- pascals-triangle.py
+++ pascals-triangle.py
@@ -1,14 +1,13 @@
 class Solution:
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        ans = []
+    def generate(self, numRows: int) -> List[List[int]]:
+        dp = []
         for i in range(numRows):
-            row = [0 for i in range(i + 1)]
-            row[0] = row[-1] = 1
-            for col in range(1, i):
-                row[col] = ans[-1][col - 1] + ans[-1][col]
-            ans.append(row)
-        return ans
+            row = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(dp[-1][j] + dp[-1][j - 1])
+            dp.append(row)
+        return dp
+
