176c7fdf9521ae729092d54f036adb5b074db91b
Wan Liuyang <tsfdye@gmail.com>
2019-03-05 00:59:56 +0800

    Update number-of-islands.py

--- number-of-islands.py
+++ number-of-islands.py
@@ -1,13 +1,8 @@
 class Solution:
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+    def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
 
-        count = 0
         rows, cols = len(grid), len(grid[0])
 
         def bfs(r, c):
@@ -16,15 +11,17 @@ class Solution:
             grid[r][c] = "0"
             while queue:
                 r, c = queue.popleft()
-                for d in ((-1, 0), (0, 1), (1, 0), (0, -1)):
-                    nr, nc = r + d[0], c + d[1]
+                for dr, dc in ((-1, 0), (0, 1), (1, 0), (0, -1)):
+                    nr, nc = dr + r, dc + c
                     if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == "1":
-                        queue.append((nr, nc))
                         grid[nr][nc] = "0"
+                        queue.append((nr, nc))
 
+        ans = 0
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == "1":
                     bfs(i, j)
-                    count += 1
-        return count
+                    ans += 1
+        return ans
+
