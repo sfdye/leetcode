b8a745dea9187ce5778ab1678d9e334867db96be
Wan Liuyang <tsfdye@gmail.com>
2019-03-05 00:01:45 +0800

    Update binary-search-tree-iterator.py

--- binary-search-tree-iterator.py
+++ binary-search-tree-iterator.py
@@ -1,40 +1,38 @@
-# Definition for a  binary tree node
-# class TreeNode(object):
+# Definition for a binary tree node.
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
 
-class BSTIterator(object):
-    def __init__(self, root):
-        """
-        :type root: TreeNode
-        """
+class BSTIterator:
+    def __init__(self, root: TreeNode):
         self.stack = []
         self._push_left(root)
 
-    def hasNext(self):
+    def next(self) -> int:
         """
-        :rtype: bool
-        """
-        return self.stack
-
-    def next(self):
-        """
-        :rtype: int
+        @return the next smallest number
         """
         node = self.stack.pop()
         if node.right:
             self._push_left(node.right)
         return node.val
 
-    def _push_left(self, node):
-        while node:
-            self.stack.append(node)
-            node = node.left
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return bool(self.stack)
+
+    def _push_left(self, root):
+        while root:
+            self.stack.append(root)
+            root = root.left
 
 
-# Your BSTIterator will be called like this:
-# i, v = BSTIterator(root), []
-# while i.hasNext(): v.append(i.next())
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
