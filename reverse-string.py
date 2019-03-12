e05efdc772cceb97311a0a3905a6fadbae81d5a9
Wan Liuyang <tsfdye@gmail.com>
2019-03-12 23:29:57 +0800

    Add reverse-string.py

---
+++ reverse-string.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s) // 2):
+            s[i], s[~i] = s[~i], s[i]
+
