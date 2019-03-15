d8f5a0ef58277e4c1822f7aada76a18f9d3ec857
Wan Liuyang <tsfdye@gmail.com>
2019-03-15 15:01:40 +0800

    Add insert-delete-get-random-o-1.py

---
+++ insert-delete-get-random-o-1.py
@@ -0,0 +1,47 @@
+class RandomizedSet:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.nums = []
+        self.pos = {}
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val not in self.pos:
+            self.nums.append(val)
+            self.pos[val] = len(self.nums) - 1
+            return True
+        else:
+            return False
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.pos:
+            p = self.pos[val]
+            last = self.nums[-1]
+            self.nums[p] = last
+            self.pos[last] = p
+            self.nums.pop()
+            del self.pos[val]
+            return True
+        else:
+            return False
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return random.choice(self.nums)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
+
