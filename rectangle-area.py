da89890929cd478a955dfde8f7b0e18542a80a7f
Wan Liuyang <tsfdye@gmail.com>
2019-03-17 18:02:02 +0800

    Update rectangle-area.py

--- rectangle-area.py
+++ rectangle-area.py
@@ -1,16 +1,8 @@
-class Solution(object):
-    def computeArea(self, A, B, C, D, E, F, G, H):
-        """
-        :type A: int
-        :type B: int
-        :type C: int
-        :type D: int
-        :type E: int
-        :type F: int
-        :type G: int
-        :type H: int
-        :rtype: int
-        """
+class Solution:
+    def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> int:
+        left = max(A, E)
+        right = max(min(C, G), left)
+        bottom = max(B, F)
+        top = max(min(D, H), bottom)
+        return (C - A) * (D - B) + (G - E) * (H - F) - (right - left) * (top - bottom)
 
-        sums = (C - A) * (D - B) + (G - E) * (H - F)
-        return sums - max(min(C, G) - max(A, E), 0) * max(min(D, H) - max(B, F), 0)
