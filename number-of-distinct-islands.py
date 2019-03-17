413abcc61cc19f7a379a62819e3dc36639d706f4
Wan Liuyang <tsfdye@gmail.com>
2019-03-17 22:05:50 +0800

    Update number-of-distinct-islands.py

--- number-of-distinct-islands.py
+++ number-of-distinct-islands.py
@@ -1,26 +1,27 @@
 class Solution:
-    def numDistinctIslands(self, grid: "List[List[int]]") -> "int":
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
         if not grid:
             return 0
-        seen = set()
+
         rows, cols = len(grid), len(grid[0])
 
         def dfs(r, c, d):
-            if 0 <= r < rows and 0 <= c < cols and grid[r][c] == 1 and (r, c) not in seen:
-                seen.add((r, c))
-                shape.append(d)
-                dfs(r + 1, c, 1)
-                dfs(r, c + 1, 2)
-                dfs(r - 1, c, 3)
-                dfs(r, c - 1, 4)
-                shape.append(-1)
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != 1:
+                return
+            grid[r][c] = 0
+            shape.append(d)
+            dfs(r + 1, c, 1)
+            dfs(r, c + 1, 2)
+            dfs(r - 1, c, 3)
+            dfs(r, c - 1, 4)
+            shape.append(-1)
 
-        shapes = set()
+        ans = set()
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 1:
                     shape = []
                     dfs(i, j, 0)
-                    if shape:
-                        shapes.add(tuple(shape))
-        return len(shapes)
+                    ans.add(tuple(shape))
+        return len(ans)
+
