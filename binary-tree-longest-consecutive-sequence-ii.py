2931cc559596cdfafc3dfd24054c08915bdbaee3
Wan Liuyang <tsfdye@gmail.com>
2019-03-17 21:48:18 +0800

    Update binary-tree-longest-consecutive-sequence-ii.py

--- binary-tree-longest-consecutive-sequence-ii.py
+++ binary-tree-longest-consecutive-sequence-ii.py
@@ -8,27 +8,22 @@
 
 class Solution:
     def longestConsecutive(self, root: TreeNode) -> int:
-        self.ans = 0
-
         def dfs(root):
             if not root:
                 return 0, 0
             else:
                 inc = dec = 1
-                if root.left:
-                    left_inc, left_dec = dfs(root.left)
-                    if root.left.val == root.val + 1:
-                        inc = left_inc + 1
-                    if root.left.val == root.val - 1:
-                        dec = left_dec + 1
-                if root.right:
-                    right_inc, right_dec = dfs(root.right)
-                    if root.right.val == root.val + 1:
-                        inc = max(inc, right_inc + 1)
-                    if root.right.val == root.val - 1:
-                        dec = max(dec, right_dec + 1)
+                for child in root.left, root.right:
+                    if child:
+                        child_inc, child_dec = dfs(child)
+                        if child.val == root.val + 1:
+                            inc = max(inc, child_inc + 1)
+                        if child.val == root.val - 1:
+                            dec = max(dec, child_dec + 1)
                 self.ans = max(self.ans, inc + dec - 1)
                 return inc, dec
 
+        self.ans = 0
         dfs(root)
         return self.ans
+
