a9bc1b9ee211ab8a3b83ca360de41fc75f52d158
Wan Liuyang <tsfdye@gmail.com>
2019-03-18 22:40:06 +0800

    Update sort-transformed-array.py

--- sort-transformed-array.py
+++ sort-transformed-array.py
@@ -1,16 +1,14 @@
 class Solution:
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
         nums = [a * x * x + b * x + c for x in nums]
-        ans = [0 for _ in range(len(nums))]
-        i, j = 0, len(nums) - 1
-        k, d = (-1, -1) if a > 0 else (0, 1)
-        while i <= j:
-            if nums[i] * -d > nums[j] * -d:
-                ans[k] = nums[i]
-                i += 1
+        left, right = 0, len(nums) - 1
+        ans = []
+        while left <= right:
+            if (a > 0) ^ (nums[left] < nums[right]):
+                ans.append(nums[left])
+                left += 1
             else:
-                ans[k] = nums[j]
-                j -= 1
-            k += d
-        return ans
+                ans.append(nums[right])
+                right -= 1
+        return ans if a <= 0 else ans[::-1]
 
