424343294709dece9a6dc2aee295cafddf111645
Wan Liuyang <tsfdye@gmail.com>
2019-03-19 00:25:11 +0800

    Update top-k-frequent-elements.py

--- top-k-frequent-words.py
+++ top-k-frequent-words.py
@@ -1,28 +1,23 @@
 class Solution:
     def topKFrequent(self, nums, k):
         def quick_select(l, r):
-            pivot = l
-            i, j = l, r
-            while i < j:
-                while i < j and counts[j][1] <= counts[pivot][1]:
-                    j -= 1
-                while i < j and counts[i][1] >= counts[pivot][1]:
+            if l >= r:
+                return
+            pivot = random.randint(l, r)
+            counts[l], counts[pivot] = counts[pivot], counts[l]
+            i = l
+            for j in range(l + 1, r + 1):
+                if counts[j][1] > counts[l][1]:
                     i += 1
-                counts[i], counts[j] = counts[j], counts[i]
+                    counts[i], counts[j] = counts[j], counts[i]
             counts[l], counts[i] = counts[i], counts[l]
-
             if i + 1 == k:
-                return counts[: i + 1]
+                return
             elif i + 1 < k:
-                return quick_select(i + 1, r)
+                quick_select(i + 1, r)
             else:
-                return quick_select(l, i - 1)
+                quick_select(l, i - 1)
 
-        if not nums:
-            return []
-
-        counts = collections.defaultdict(int)
-        for num in nums:
-            counts[num] += 1
-        counts = list(counts.items())
-        return [c[0] for c in quick_select(0, len(counts) - 1)]
+        counts = list(collections.Counter(nums).items())
+        quick_select(0, len(counts) - 1)
+        return [c[0] for c in counts[:k]]
