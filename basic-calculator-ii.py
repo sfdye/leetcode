ce801bda89d0838d2308c98a1e23f40dc6165030
Wan Liuyang <tsfdye@gmail.com>
2019-03-19 23:29:21 +0800

    Update basic-calculator-ii.py

--- basic-calculator-ii.py
+++ basic-calculator-ii.py
@@ -1,19 +1,22 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack, sign, num = [], "+", 0
+        pre, cur, ans, sign = 0, 0, 0, "+"
         s += "$"
         for c in s:
             if c.isdigit():
-                num = num * 10 + int(c)
+                cur = cur * 10 + int(c)
             elif c in "+-*/$":
                 if sign == "+":
-                    stack.append(num)
+                    ans += pre
+                    pre = cur
                 elif sign == "-":
-                    stack.append(-num)
+                    ans += pre
+                    pre = -cur
                 elif sign == "*":
-                    stack.append(stack.pop() * num)
+                    pre *= cur
                 else:
-                    stack.append(int(stack.pop() / num))
-                num = 0
+                    pre = int(pre / cur)
+                cur = 0
                 sign = c
-        return sum(stack)
+        return ans + pre
+
