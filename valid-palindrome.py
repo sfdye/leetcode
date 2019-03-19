4e0c1eec1c479aba016db7e47d2a59faadfe8c13
Wan Liuyang <tsfdye@gmail.com>
2019-03-19 21:27:43 +0800

    Update valid-palindrome.py

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -1,8 +1,14 @@
 class Solution:
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        s = "".join([c for c in s.lower() if c.isalnum()])
-        return all(s[i] == s[~i] for i in range(len(s) // 2))
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True
+
