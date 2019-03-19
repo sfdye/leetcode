750d0541c012672983e8c7118f6f3dfc9b803bdb
Wan Liuyang <tsfdye@gmail.com>
2019-03-19 23:30:23 +0800

    Update basic-calculator.py

--- basic-calculator.py
+++ basic-calculator.py
@@ -1,26 +1,21 @@
 class Solution:
-    def calculate(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        num, stack, sign = 0, [], "+"
-        s += " "
-        i = 0
-        while i < len(s):
-            if s[i].isdigit():
-                num = num * 10 + int(s[i])
-            elif s[i] == "(":
-                num, skip = self.calculate(s[i + 1 :])
-                i += skip
-            elif s[i] in "+-)" or i == len(s) - 1:
-                if sign == "+":
-                    stack.append(num)
-                elif sign == "-":
-                    stack.append(-num)
-                if s[i] == ")":
-                    return sum(stack), i + 1
+    def calculate(self, s: str) -> int:
+        res, num, sign, stack = 0, 0, 1, []
+        for c in s:
+            if c.isdigit():
+                num = num * 10 + int(c)
+            elif c in "+-":
+                res += sign * num
+                sign = 1 if c == "+" else -1
                 num = 0
-                sign = s[i]
-            i += 1
-        return sum(stack)
+            elif c == "(":
+                stack.append(res)
+                stack.append(sign)
+                res, sign = 0, 1
+            elif c == ")":
+                res += sign * num
+                num = 0
+                res *= stack.pop()
+                res += stack.pop()
+        return res + sign * num
+
