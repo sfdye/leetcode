755ef7bddc06e678ee47934d24389e298ffc7cf2
Wan Liuyang <tsfdye@gmail.com>
2019-03-23 20:23:52 +0800

    Add longest-substring-with-at-most-two-distinct-characters.py

--- longest-substring-with-at-most-two-distinct-characters.py
+++ longest-substring-with-at-most-two-distinct-characters.py
@@ -1,11 +1,11 @@
 class Solution:
-    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
         d = {}
         left = right = maxlen = 0
         while right < len(s):
             d[s[right]] = right
             right += 1
-            if len(d) == k + 1:
+            if len(d) == 3:
                 min_left = min(d.values())
                 left = min_left + 1
                 del d[s[min_left]]
