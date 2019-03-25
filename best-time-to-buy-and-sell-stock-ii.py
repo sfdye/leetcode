fc145644854fc04e8fea06768260ba11bb58d322
Wan Liuyang <tsfdye@gmail.com>
2019-03-25 21:42:08 +0800

    Update best-time-to-buy-and-sell-stock-ii.py

--- best-time-to-buy-and-sell-stock-ii.py
+++ best-time-to-buy-and-sell-stock-ii.py
@@ -1,16 +1,8 @@
 class Solution:
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-
-        if not prices:
-            return 0
-
-        buy = -prices[0]
-        sell = 0
+    def maxProfit(self, prices: List[int]) -> int:
+        ans = 0
         for i in range(1, len(prices)):
-            buy = max(buy, sell - prices[i])
-            sell = max(sell, buy + prices[i])
-        return sell
+            if prices[i] > prices[i - 1]:
+                ans += prices[i] - prices[i - 1]
+        return ans
+
