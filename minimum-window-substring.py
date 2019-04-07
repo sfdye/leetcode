22247bf72a7dd91437158f39e0b0351f95552c9c
Wan Liuyang <tsfdye@gmail.com>
2019-04-07 20:29:42 +0800

    Update minimum-window-substring.py

--- minimum-window-substring.py
+++ minimum-window-substring.py
@@ -1,27 +1,22 @@
 class Solution:
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+    def minWindow(self, s: str, t: str) -> str:
         left = right = formed = 0
         ans = float("inf"), 0, 0
         cur = collections.defaultdict(int)
         expected = collections.Counter(t)
-        required = len(expected)
         while right < len(s):
-            ch = s[right]
-            cur[ch] += 1
-            if ch in expected and cur[ch] == expected[ch]:
+            c = s[right]
+            cur[c] += 1
+            if cur[c] == expected[c]:
                 formed += 1
-            while left <= right and formed == required:
+            while left <= right and formed == len(expected):
                 if right - left + 1 < ans[0]:
                     ans = right - left + 1, left, right
-                ch = s[left]
-                cur[ch] -= 1
-                if ch in expected and cur[ch] < expected[ch]:
+                c = s[left]
+                cur[c] -= 1
+                if cur[c] < expected[c]:
                     formed -= 1
                 left += 1
             right += 1
         return s[ans[1] : ans[2] + 1] if ans[0] < float("inf") else ""
+
