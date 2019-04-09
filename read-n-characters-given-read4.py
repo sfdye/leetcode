71723a0de74216b37a8dd9f59a95fe11cb84d7fd
Wan Liuyang <tsfdye@gmail.com>
2019-04-09 23:25:49 +0800

    Update read-n-characters-given-read4.py

--- read-n-characters-given-read4.py
+++ read-n-characters-given-read4.py
@@ -1,23 +1,35 @@
-# The read4 API is already defined for you.
-# @param buf, a list of characters
-# @return an integer
-# def read4(buf):
+"""
+The read4 API is already defined for you.
+
+    @param buf, a list of characters
+    @return an integer
+    def read4(buf):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
 
 
-class Solution(object):
+class Solution:
+    def __init__(self):
+        self.buf4 = [""] * 4
+
     def read(self, buf, n):
         """
         :type buf: Destination buffer (List[str])
-        :type n: Maximum number of characters to read (int)
-        :rtype: The number of characters read (int)
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
         """
-        count = 0
-        buf4 = [""] * 4
-        while n > 0:
-            now = min(n, read4(buf4))
-            if now == 0:
+        cur = 0
+        while cur < n:
+            k = min(read4(self.buf4), n - cur)
+            if k == 0:
                 break
-            buf[count : count + now] = buf4[:now]
-            count += now
-            n -= now
-        return count
+            buf[cur : cur + k] = self.buf4[:k]
+            cur += k
+        return cur
+
