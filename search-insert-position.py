346f163449f5eddc38bb22dddeafdc28ccb1ec40
Wan Liuyang <tsfdye@gmail.com>
2019-04-20 19:53:30 +0800

    Update search-insert-position.py

--- search-insert-position.py
+++ search-insert-position.py
@@ -1,17 +1,12 @@
 class Solution:
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        lo, hi = 0, len(nums) - 1
-        while lo <= hi:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        lo, hi = 0, len(nums)
+        while lo < hi:
             mid = (lo + hi) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
                 lo = mid + 1
             else:
-                hi = mid - 1
+                hi = mid
         return lo
