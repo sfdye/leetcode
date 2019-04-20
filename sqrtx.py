789ba42da2a1877efc0e6bd8536a153d9fc9a395
Wan Liuyang <tsfdye@gmail.com>
2019-04-20 20:08:01 +0800

    Update sqrtx.py

--- sqrtx.py
+++ sqrtx.py
@@ -1,11 +1,13 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        lo, hi = 0, x
-        while lo <= hi:
+        lo, hi = 0, x + 1
+        while lo < hi:
             mid = (lo + hi) // 2
-            if mid ** 2 <= x < (mid + 1) ** 2:
+            if mid ** 2 == x:
                 return mid
-            elif mid ** 2 > x:
-                hi = mid - 1
-            else:
+            elif mid ** 2 < x:
                 lo = mid + 1
+            else:
+                hi = mid
+        return lo - 1
+
