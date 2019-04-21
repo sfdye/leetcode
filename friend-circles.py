f2ab4378513db5f38b780d3164099db4ac5bc2b8
Wan Liuyang <tsfdye@gmail.com>
2019-04-21 11:57:44 +0800

    Update friend-circles.py

--- friend-circles.py
+++ friend-circles.py
@@ -1,19 +1,33 @@
+class DSU:
+    def __init__(self, N):
+        self.parent = list(range(N))
+        self.size = [1 for _ in range(N)]
+        self.count = N
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        x, y = self.find(x), self.find(y)
+        if x == y:
+            return
+        if self.size[x] < self.size[y]:
+            x, y = y, x
+        self.parent[y] = x
+        self.size[x] += self.size[y]
+        self.count -= 1
+
+
 class Solution:
-    def findCircleNum(self, M):
-        """
-        :type M: List[List[int]]
-        :rtype: int
-        """
-        n = len(M)
-        p = list(range(n))
-
-        def find(u):
-            if p[u] != u:
-                p[u] = find(p[u])
-            return p[u]
-
-        for i in range(n):
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        if not M:
+            return 0
+        dsu = DSU(len(M))
+        for i in range(len(M)):
             for j in range(i):
-                if M[i][j]:
-                    p[find(i)] = p[find(j)]
-        return len(set(map(find, p)))
+                if M[i][j] == 1:
+                    dsu.union(i, j)
+        return dsu.count
+
