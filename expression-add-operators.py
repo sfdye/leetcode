1075183c7f7013a33e11daafbe5663a2941b9199
Wan Liuyang <tsfdye@gmail.com>
2019-04-21 22:04:30 +0800

    Update expression-add-operators.py

--- expression-add-operators.py
+++ expression-add-operators.py
@@ -1,21 +1,19 @@
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
-        def dfs(pos, path, val, pre):
-            if pos == len(num):
-                if val == target:
+        def dfs(num, path, cur, pre):
+            if not num:
+                if cur == target:
                     self.ans.append(path)
             else:
-                for i in range(pos, len(num)):
-                    if num[pos] == "0" and i != pos:
-                        break
-                    cur = int(num[pos : i + 1])
-                    if pos == 0:
-                        dfs(i + 1, path + str(cur), cur, cur)
-                    else:
-                        dfs(i + 1, path + "+" + str(cur), val + cur, cur)
-                        dfs(i + 1, path + "-" + str(cur), val - cur, -cur)
-                        dfs(i + 1, path + "*" + str(cur), val - pre + pre * cur, cur * pre)
+                for i in range(1, len(num) + 1):
+                    if i == 1 or (i > 1 and num[0] != "0"):
+                        val = int(num[:i])
+                        dfs(num[i:], path + "+" + str(val), cur + val, val)
+                        dfs(num[i:], path + "-" + str(val), cur - val, -val)
+                        dfs(num[i:], path + "*" + str(val), cur - pre + pre * val, pre * val)
 
         self.ans = []
-        dfs(0, "", 0, 0)
+        for i in range(1, len(num) + 1):
+            if i == 1 or (i > 1 and num[0] != "0"):
+                dfs(num[i:], num[:i], int(num[:i]), int(num[:i]))
         return self.ans
