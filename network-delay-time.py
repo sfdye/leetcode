0fdbb13d861957ac805425187466427b57860895
Wan Liuyang <tsfdye@gmail.com>
2019-04-29 21:57:15 +0800

    Add network-delay-time.py

---
+++ network-delay-time.py
@@ -0,0 +1,16 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        dist = [float("inf") for _ in range(N)]
+        K -= 1
+        dist[K] = 0
+        weight = collections.defaultdict(dict)
+        for u, v, w in times:
+            weight[u - 1][v - 1] = w
+        queue = collections.deque([K])
+        while queue:
+            u = queue.popleft()
+            for v in weight[u]:
+                if dist[u] + weight[u][v] < dist[v]:
+                    dist[v] = dist[u] + weight[u][v]
+                    queue.append(v)
+        return max(dist) if max(dist) < float("inf") else -1
